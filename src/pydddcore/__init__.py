5567c1977b1fd3deb5314f5537c9c31ba1ffdbfb
Kapil Dhaimade <kapil.dhaimade@live.com>
2024-04-16 01:15:59 +0530

    Refactor __init__.py to fix lint error

--- src/pydddcore/__init__.py
+++ src/pydddcore/__init__.py
@@ -1,6 +1,4 @@
 from .core import EntityId, Entity, ValueObject, AggregateRoot, DomainEvent, DomainEventPublisher, DomainService, Repository
 
 __all__ = ['EntityId', 'Entity', 'ValueObject', 'AggregateRoot', 'DomainEvent', 'DomainEventPublisher', 'DomainService', 'Repository',
-           'DomainEventSubscriber', 'DomainException', 'core']
-
-del core
+           'DomainEventSubscriber', 'DomainException']
